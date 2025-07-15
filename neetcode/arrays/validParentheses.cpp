58d0546add46f710bbd362ca622ec687ad877934
Taina <taina_bastosalmeida@hotmail.com>
2025-07-14 21:12:00 -0300

    valid parentheses

--- neetcode/arrays/validParentheses.cpp
+++ neetcode/arrays/validParentheses.cpp
@@ -14,15 +14,19 @@ public:
         bool stringValida = false;
 
         for(int i=0; i<input.size(); i++) {
+            // verifies if it is an open bracket
             if(openChars.find(input[i]) != string::npos) {
                 stackChars.push(input[i]);
             }
 
+            // closed bracket
             if(closedChars.find(input[i]) != string::npos) {
                 if(stackChars.empty()) {
                     return false;
                 }
+
                 for(int j=0; j<3; j++) {
+                    // checks if the char on top of the stack matches the char on input[i]
                     if(pairs[j] == (string(1, stackChars.top()) + string(1, input[i]))) {
                         encontrou = true;
                         break;
@@ -38,6 +42,7 @@ public:
         }
 
         if(stackChars.empty()) {
+            // for it to be valid, the stack must end empty
             stringValida = true;
         }
 
@@ -49,7 +54,6 @@ public:
 
 int main() {
     Solution sol;
-    // reads a string input
     string input;
     cin >> input;
 
