b69792d93554865c06adcad61e6f9b0067a479c5
Tainá Almeida <taina_bastosalmeida@hotmail.com>
2026-08-10 15:05:24 -0300

    two sum ordered vector

---
+++ neetcode/two_pointers/medium/two_sum_sorted.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int numbers_len = numbers.size();
+        vector<int> indices(2, 0);
+
+        // binary search
+        int left = 0, right = numbers_len - 1;
+        while(left <= right) {
+            int middle = (left+right)/2;
+            if(numbers[left] + numbers[right] == target) {
+                indices[0] = left + 1;
+                indices[1] = right + 1;
+                return indices;
+            } else if(numbers[left] + numbers[middle] > target) {
+                right = middle;
+            } else if(numbers[left] + numbers[middle] < target) {
+                left = middle;
+            } else {
+                indices[0] = left + 1;
+                indices[1] = middle + 1;
+                return indices;
+            }
+        }
+        return indices;
+
+        // // brute force
+        // vector<int> indices(2, 0);
+        // int counter = 0;
+        // for(int i = 0; i<numbers_len-1; i++) {
+        //     for(int j=i+1; j<numbers_len; j++) {
+        //         if(numbers[i] + numbers[j] == target) {
+        //             indices[0] = i + 1;
+        //             indices[1] = j + 1;
+        //             return indices;
+        //         }
+        //     }
+        // }
+        // return indices;
+    }
+};
+
+int main() {
+    Solution s = Solution();
+    vector<int> numbers = {1, 9, 13, 14};
+    int target = 23;
+
+    vector<int> res = s.twoSum(numbers, target);
+
+    cout << res[0] << ' ' << res[1] << endl;
+}
