207828511521ff9507fb454d05364cd3c608e0ec
Tainá Almeida <taina_bastosalmeida@hotmail.com>
2026-08-11 23:44:10 -0300

    longest repeating character replacement

--- neetcode/sliding_window/medium/longest_repeating.cpp
+++ neetcode/sliding_window/medium/longest_repeating.cpp
@@ -12,23 +12,19 @@ public:
         int max_len = 0;
 
         int left = 0, right = 0;
-        // if(s[right]) {
-        //     frequency_chars[s[right]]++;
-        // }
-
         while(right < s.size()) {
+            frequency_chars[s[right]]++;
             max_frequency = max(max_frequency, frequency_chars[s[right]]);
             int replacements = (right - left + 1) - max_frequency;
             if(replacements > k) {
                 frequency_chars[s[left]]--;
                 left++;
-            } else {
-                frequency_chars[s[right]]++;
-                right++;
-            }
+            } 
             if((right - left + 1) > max_len) {
                 max_len = (right - left + 1);
             }
+
+            right++;
             
         }
         return max_len;
